008b2c7932ac46596f74b4dc444f2dc7eac444db
Pankaj Rajdeo <pankajrajdeo@gmail.com>
2025-08-22 20:03:14 -0400

    Update __init__.py

--- bond/__init__.py
+++ bond/__init__.py
@@ -1,3 +1,6 @@
+from .runtime_env import configure_runtime
+configure_runtime()  # ensure OpenMP env is set before any heavy imports
+
 from .pipeline import BondMatcher
 
 __all__ = ["BondMatcher"]
